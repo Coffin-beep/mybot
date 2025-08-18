cbd7c0973b599606c7203f163ff07b4e988e2c0d
Coffin-beep <ilyusha1677@yandex.ru>
2025-08-18 16:26:09 +0700

    11

--- bot1mobile.py
+++ bot1mobile.py
@@ -2,9 +2,10 @@ from telegram import Update
 from telegram.ext import Application, CommandHandler, MessageHandler, filters, CallbackContext
 
 #Токен бота
-from config import TOKEN1
+from config import TOKEN1, MY_ID_CHAT
 
 TOKEN = TOKEN1
+ADMIN_ID_CHAT = MY_ID_CHAT
 
 async def start(update: Update, context: CallbackContext) -> None:
     await update.message.reply_text('Илюха, привет!')
--- config.py
+++ config.py
@@ -5,3 +5,4 @@ from dotenv import load_dotenv
 load_dotenv()
 
 TOKEN1 = os.getenv('TOKEN_BOT_1')
+MY_ID_CHAT = os.getenv('MY_ID_CHAT')
